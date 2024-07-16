52baa135865e18fcab43cf9f5df4eb73c9447d8e
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-07-16 11:26:46 +0900

    トランザクション処理

--- api/src/app/Http/Controllers/MailController.php
+++ api/src/app/Http/Controllers/MailController.php
@@ -6,7 +6,10 @@ use App\Http\Resources\MailResource;
 use App\Models\HaveItem;
 use App\Models\Mail;
 use App\Models\UserMail;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Mockery\Exception;
+use Illuminate\Support\Facades\DB;
 
 class MailController extends Controller
 {
@@ -30,43 +33,51 @@ class MailController extends Controller
     //メール受け取り処理
     public function openMail(Request $request)
     {
-        //UserMailからuser_idとmail_idが一致するデータを取得
-        $userMails = UserMail::where('user_id', '=', $request->user_id)
-            ->where('mail_id', '=', $request->mail_id)
-            ->get();
+        try {
+            //トランザクション処理
+            DB::transaction(function () use ($request) {
+                //UserMailからuser_idとmail_idが一致するデータを取得
+                $userMails = UserMail::where('user_id', '=', $request->user_id)
+                    ->where('mail_id', '=', $request->mail_id)
+                    ->get();
 
-        //データがなかったらエラー
-        if (count($userMails) === 0) {
-            return response()->json([], 400);
-        }
+                //データがなかったらエラー
+                if (count($userMails) === 0) {
+                    return response()->json([], 400);
+                }
 
-        //カラム更新
-        if ($userMails->first()->open_flag === 1) {
-            //もう開封済みだったらエラー
-            return response()->json([], 400);
-        }
-        $userMails->first()->open_flag = 1;
+                //カラム更新
+                if ($userMails->first()->open_flag === 1) {
+                    //もう開封済みだったらエラー
+                    return response()->json([], 400);
+                }
+                $userMails->first()->open_flag = 1;
 
-        //保存
-        $userMails->first()->save();
+                //保存
+                $userMails->first()->save();
 
-        $mails = Mail::findOrFail($request->mail_id);
+                $mails = Mail::findOrFail($request->mail_id);
 
-        $userItems = HaveItem::where('user_id', '=', $request->user_id)
-            ->where('item_id', '=', $mails->item_id)
-            ->get();
+                $userItems = HaveItem::where('user_id', '=', $request->user_id)
+                    ->where('item_id', '=', $mails->item_id)
+                    ->get();
 
-        if (count($userItems) === 0) {
-            $items = HaveItem::create([
-                'user_id' => $request->user_id,
-                'item_id' => $mails->item_id,
-                'possession' => $mails->amount
-            ]);
-        } else {
-            $userItems->first()->possession += $mails->amount;
+                if (count($userItems) === 0) {
+                    $items = HaveItem::create([
+                        'user_id' => $request->user_id,
+                        'item_id' => $mails->item_id,
+                        'possession' => $mails->amount
+                    ]);
+                } else {
+                    $userItems->first()->possession += $mails->amount;
 
-            //保存
-            $userItems->first()->save();
+                    //保存
+                    $userItems->first()->save();
+                }
+                return response()->json();
+            });
+        } catch (Exception $e) {
+            return response()->json($e, 500);
         }
     }
 }
--- api/src/app/Http/Controllers/UserController.php
+++ api/src/app/Http/Controllers/UserController.php
@@ -9,6 +9,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Mockery\Exception;
 
 class UserController extends Controller
 {
@@ -84,29 +86,46 @@ class UserController extends Controller
             return response()->json($validator->errors(), 400);
         }
 
-        $user = User::create([
-            'name' => $request->name,
-            'level' => 1,
-            'experience_point' => 1,
-            'life' => 1
-        ]);
+        try {
+            //トランザクション処理
+            $user = DB::transaction(function () use ($request) {
+                $user = User::create([
+                    'name' => $request->name,
+                    'level' => 1,
+                    'experience_point' => 1,
+                    'life' => 1
+                ]);
+                return $user;
+            });
+            return response()->json($user);
+        } catch (Exception $e) {
+            return response()->json($e, 500);
+        }
 
-        return response()->json(['user_id' => $user->id]);
+
     }
 
     //ユーザー更新
     public function update(Request $request)
     {
-        $user = User::findOrFail($request->user_id);
+        try {
+            //トライザクション処理
+            DB::transaction(function () use ($request) {
+                $user = User::findOrFail($request->user_id);
 
-        //カラムの更新
-        if (isset($request->level)) {
-            $user->level = $request->level;
+                //カラムの更新
+                if (isset($request->level)) {
+                    $user->level = $request->level;
+                }
+
+                //保存
+                $user->save();
+            });
+            return response()->json();
+        } catch (Exception $error) {
+            return response()->json($error, 500);
         }
-
-        //保存
-        $user->save();
     }
 
-    
+
 }
